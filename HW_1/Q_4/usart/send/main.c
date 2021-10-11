70c18e818c3aba2cba636799c510e1edf5f9b44a
zahra996 <59256236+zahra996@users.noreply.github.com>
2021-10-12 01:46:18 +0330

    Add files via upload

---
+++ HW_1/Q_4/usart/send/main.c
@@ -0,0 +1,33 @@
+/*
+ * send.c
+ *
+ * Created: 20/07/1400 12:03:36 ق.ظ
+ * Author : New Tech
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "global.h"
+#include <util/delay.h>
+#include "usart.h"
+
+int i = 0;
+
+int main(void)
+{
+    /* Replace with your application code */
+	uart_init(BAUD_CALC(115200));
+	sei();
+	_delay_ms(1000);
+	uart_puts("faradars\r\n");
+	_delay_ms(1000);
+	
+    while (1) 
+    {
+		uart_putint(i);
+		uart_puts("\r\n");
+		_delay_ms(1000);
+		i++;
+    }
+}
+
