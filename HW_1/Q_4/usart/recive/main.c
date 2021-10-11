7bf9d568ca2f72e8095499e01dcc909810be4dfc
zahra996 <59256236+zahra996@users.noreply.github.com>
2021-10-12 01:50:21 +0330

    Add files via upload

---
+++ HW_1/Q_4/usart/recive/main.c
@@ -0,0 +1,45 @@
+/*
+ * recive.c
+ *
+ * Created: 20/07/1400 12:04:46 ق.ظ
+ * Author : New Tech
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "global.h"
+#include <util/delay.h>
+#include "usart.h"
+
+#include "lcd.h"
+#include "rprintf.h"
+
+char buffer[200];
+
+int main(void)
+{
+    /* Replace with your application code */
+	uart_init(BAUD_CALC(115200));
+	sei();
+	 
+	stdout = &uart0_io;
+	stdin = &uart0_io;
+	
+	lcdInit();
+	rprintfInit(lcdDataWrite);
+	
+	lcdClear();
+	lcdGotoXY(0,0);
+	rprintf("ready");
+	_delay_ms(200);
+	
+    while (1) 
+    {
+		scanf("%s",buffer);
+		lcdClear();
+		lcdGotoXY(0,0);
+		rprintfStr(buffer);
+		_delay_ms(200);
+    }
+}
+
